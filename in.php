b1b1ad0c7a0628d747dd5cce13147690a421c863
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2024-05-23 11:53:34 +0300

    introducing php

---
+++ in.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "this is my first php code";
+
+print "<br>";
+
+print "Today is <span style='colour: #ff4856; 
+text-'>Thursday";
+
+print "<br>"; //Using HTML's <br> to break a line
+
+print 1999; //This is an integer or a digit
+
+print "<br>"; //Using HTML's <br> to break a line
+
+print "5475"; //This is a string
+
+print "<br>"; //Using HTML's <br> to break a line
+
+print date('1', srtotime('+125 days'));
+
+?>
