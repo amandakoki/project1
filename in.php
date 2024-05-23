217c43570faf081c54dbe7882f71b19a63a4ac34
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2024-05-23 12:12:07 +0300

    using strtotime function to display date

--- in.php
+++ in.php
@@ -1,12 +1,12 @@
 <?php
 
 date_default_timezone_set("Africa/Nairobi");
+
 echo "this is my first php code";
 
 print "<br>";
 
-print "Today is <span style='colour: #ff4856; 
-text-'>Thursday";
+print "Today is <span style='colour: #ff4856; text-transform: uppercase;'>Thursday</span>";
 
 print "<br>"; //Using HTML's <br> to break a line
 
@@ -18,7 +18,11 @@ print "5475"; //This is a string
 
 print "<br>"; //Using HTML's <br> to break a line
 
-print date('1, F jS Y', srtotime('+5 months'));
+print date('1'); //Displaying the current day of the week
+
+print "<br>"; //Using HTML's <br> to break a line
+
+print date('1, F jS Y', strtotime('+5 months'));
 
 print "<br>"; //Using HTML's <br> to break a line
 
