2579998d0957832de401bab570825e215f5c3867
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2024-06-26 00:54:16 +0300

    removal of repeated toplink code from the signup page

--- signup.php
+++ signup.php
@@ -9,19 +9,6 @@
 </head>
 <body>
 
-    <div class="toplink">
-        <a href="./index.html">Home</a>
-        <a href="news.html">News</a>
-        <a href="about_us.html">About Us</a>
-        <a href="contact_us.html">Contact Us</a>
-        <a href="my_books.html">My Books</a>
-
-        <div class="toplink-right">
-        <a href="signup.html">Sign Up</a>
-        <a href="login.html">Log in</a>
-        </div>
-    </div>
-
     
     <div class="banner">
         <h1 style="font-style:inherit; color:black ;">Sign Up</h1>
