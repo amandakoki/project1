1f78453fb035df37a5550c3d64d4af4ebb6bcb07
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2024-06-26 01:00:49 +0300

    editing the my books.html to my books.php in toplinks under nav templates

--- templates/nav.php
+++ templates/nav.php
@@ -4,7 +4,7 @@
             <a href="news.php">News</a>
             <a href="about_us.php">About Us</a>
             <a href="contact_us.php">Contact Us</a>
-            <a href="my_books.html">My Books</a>
+            <a href="my_books.php">My Books</a>
 
             <div class="toplink-right">
             <a href="signup.html">Sign up</a>
