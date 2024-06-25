20c56333a475c7819c3cc078ebd57ea3f42f4a8a
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2024-06-26 00:43:08 +0300

    include_once code for nav template

--- index.php
+++ index.php
@@ -8,19 +8,7 @@
 
     </head>
     <body>
-        
-        <div class="toplink">
-            <a href="./index.html">Home</a>
-            <a href="news.html">News</a>
-            <a href="about_us.html">About Us</a>
-            <a href="contact_us.html">Contact Us</a>
-            <a href="my_books.html">My Books</a>
-
-            <div class="toplink-right">
-            <a href="signup.html">Sign up</a>
-            <a href="login.html">Log in</a>
-            </div>
-        </div>
+    <?php include_once ("templates/nav.php"); ?>
         
         <div class="banner">
             <h1 style="font-style:inherit; color:black ;">Welcome to The Rkive</h1>
