9206fa9a937485c4ee1653dcc05d7139975bdb32
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2024-06-26 01:03:03 +0300

    adding the nav templates toplink code to the about us page

--- about_us.php
+++ about_us.php
@@ -8,7 +8,8 @@
 
 </head>
 <body>
-    <?php include_once ("templates/nav.php"); ?>
+    
+<?php include_once ("templates/nav.php"); ?>
 
     <div class="banner">
         <h1 style="font-style:inherit; color:#000000 ;">About Us</h1>
