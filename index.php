3aa9acd3a3cda72ad2520bae5e07f2ad29c73670
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2024-06-26 01:10:41 +0300

    using include_once code to access heading.php code

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang ="en">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html";
-        charset="UTF-8">
-        <title>Welcome -Document</title>
-        <link rel="stylesheet" href="style.css">
-
-    </head>
-    <body>
+<?php include_once ("templates/heading.php"); ?>
     <?php include_once ("templates/nav.php"); ?>
         
         <div class="banner">
