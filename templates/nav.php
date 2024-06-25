57d5fb9a63943d3b0347f7bc9b967c4c9843e842
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2024-06-26 00:38:57 +0300

    storing similar toplink code in nav template

---
+++ templates/nav.php
@@ -0,0 +1,14 @@
+
+<div class="toplink">
+            <a href="./index.html">Home</a>
+            <a href="news.html">News</a>
+            <a href="about_us.html">About Us</a>
+            <a href="contact_us.html">Contact Us</a>
+            <a href="my_books.html">My Books</a>
+
+            <div class="toplink-right">
+            <a href="signup.html">Sign up</a>
+            <a href="login.html">Log in</a>
+            </div>
+        </div>
+        
