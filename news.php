0ec1a6cdd30ebf64193c9de87ff5580631e4f734
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2024-06-26 01:12:02 +0300

    using the inlcude_once code to access nav templates

--- news.php
+++ news.php
@@ -9,20 +9,8 @@
 </head>
 <body>
 
-    <div class="toplink">
-        <a href="./index.html">Home</a>
-        <a href="news.html">News</a>
-        <a href="about_us.html">About Us</a>
-        <a href="contact_us.html">Contact Us</a>
-        <a href="my_books.html">My Books</a>
+<?php include_once ("templates/nav.php"); ?>
 
-        <div class="toplink-right">
-        <a href="signup.html">Sign up</a>
-        <a href="login.html">Log in</a>
-        </div>
-    </div>
-
-    
     <div class="banner">
         <h1 style="font-style:inherit; color:black ;">News</h1>
     </div>
