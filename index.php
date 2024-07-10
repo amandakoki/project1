5c2bb55114fdb5a34c0c1598f78db0620b7832b1
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2024-07-10 19:58:56 +0300

    fixing the sidebar

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
         <div class="banner">
             <h1 style="font-style:inherit; color:black ;">Welcome to The Rkive</h1>
         </div>
+        <div class="row">
+            <div class="content">
 
         <p>The Rkive is an online library service that allows avid readers, people with general interest in books,
             academics and new readers alike to engage with literature of all kinds. It is fully accessible for use by all
@@ -13,10 +15,7 @@
             You can find out more about The Archive in our About Us page !<p>
         
         <img src="images\pexels-library-img.jpg" alt="" width="30%">
-
-    <br>
+            </div>
     <?php include_once ("templates/side_bar.php");?>
+            </div>
     <?php include_once("templates/footer.php"); ?>
-    <br><br>
-    </body>
-</html>
