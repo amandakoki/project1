fadf5992e714027c1eeff539ef61ea765bc063ed
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2024-07-10 23:44:37 +0300

    creation of arrays file

--- about_us.php
+++ about_us.php
@@ -13,7 +13,9 @@
         <div class="content">
             <h1>About Us</h1>
             <img src="images\bookswhand.jpg" height="400" alt="">
-            <p>The Rkive was founded with the purpose of easing worldwide access to books for people of all ages.</p>
+            <p>The Rkive was founded with the purpose of easing worldwide access to books for people of all ages.
+                It is an online library service founded in 2024.
+            </p>
 
             <ul>
                 Programming languages used:
@@ -37,12 +39,6 @@
             </ul>
         </div>
     </div>
-
-    <h1>FAQs</h1>
-    <h2>Where are you located?</h2>
-    <p>The Rkive has branches in Kenya, Korea, Japan, France and Germany. We are currently in the process of putting up 
-        more branches of our library in China and Italy. Members will be notified upon their completion.
-    </p>
 
     <?php include_once("templates/footer.php"); ?>
     
